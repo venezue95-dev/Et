ae36efd41d4d42e46aaa222dff57cd929f09de1b
venezue95-dev <venezue95@gmail.com>
2026-08-16 01:48:05 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ AVAILABLE_CLOUDS = [
         "moodle_repo_id": 5,
         "moodle_user": "eliel15",
         "moodle_password": "ElielEliel1515.",
-        "zips": 300,
+        "zips": 99,
         "uploadtype": "evidence",
         "proxy": "",
         "tokenize": 0
