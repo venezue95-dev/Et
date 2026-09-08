8519138b05b3d948f443703b7d0528beb237a692
venezue95-dev <venezue95@gmail.com>
2026-09-08 04:24:40 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ AVAILABLE_CLOUDS = [
         "moodle_user": "daniela.martinez",
         "moodle_password": "Zenia*07",
         "zips": 99,
-        "uploadtype": "evidence",
+        "uploadtype": "draft",
         "proxy": "",
         "tokenize": 0
     },
