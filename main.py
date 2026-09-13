ec46b66a43a499d33376327292178e4013a4c95a
venezue95-dev <venezue95@gmail.com>
2026-09-13 04:40:48 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1883,7 +1883,7 @@ def onmessage(update,bot:ObigramClient):
                 print(str(ex))
             return
 
-        message = bot.sendMessage(chat_id,'<b>Procesando...</b>', parse_mode='html')
+        message = bot.sendMessage(chat_id,'<b>✨Procesando solicitud...✨</b>', parse_mode='html')
         thread.store('msg',message)
 
         # ============================================
