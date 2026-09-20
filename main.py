3f5940d0fd39ecc4b2e5c751329f4e07549008b3
venezue95-dev <venezue95@gmail.com>
2026-09-20 04:48:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ AVAILABLE_CLOUDS = [
     {
         "cloudtype": "moodle",
         "moodle_host": "https://aula.uclv.edu.cu/",
-        "moodle_repo_id": 4,
+        "moodle_repo_id": 5,
         "moodle_user": "lircarrasco",
         "moodle_password": "ElielThali15152121.",
         "zips": 79,
